34fb914ba0ad96d1679b982720fae87edbd49b2b
Cwell21 <cwe11@yahoo.com>
2018-04-28 16:28:22 -0600

    Progress commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Javascript file for FESC project 4-28-18
+
+function dispUniv() {  
+  let univ = ["uf", "fsu", "ucf", "usf", "fam", "fau", "fgc", "fiu", "ncf", "unf", "uwf", "fpu"];
+  //get DIV element
+  e = document.getElementById("partUniv");
+  var univIcon = "<li><img src=\"images\\univ\\" + univ[0] + ".jpg\" alt=\" " + univ[0] + "\"></li>" 
+  for (i=1; i<univ.length; i++) {
+   univIcon = univIcon + "<li><img src=\"images\\univ\\" + univ[i] + ".jpg\" alt=\" " + univ[i] + "\"></li>" 
+  }    
+  e.innerHTML = univIcon;
+}
+
