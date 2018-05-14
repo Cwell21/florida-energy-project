16af7d6f1e1950ad09e9defd001cb9c05828eb22
Cwell21 <cwe11@yahoo.com>
2018-05-13 22:06:33 -0600

    Drop menu feature functioning.  Needs some additional styling before release

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //Javascript file for FESC project 4-28-18
 
+//create variable to hold state of clickable menu
 var dispFlag = false;
 
 function dispUniv() {  
@@ -13,14 +14,49 @@ function dispUniv() {
   e.innerHTML = univIcon;
 }
 
-function dispMenu() {
-    var x = document.getElementById("projects");
-  if (dispFlag == false){
-    x.style.display = "block";    
-    dispFlag = true;
-  }else {
-    x.tyle.display = "none";
-    dispFlag = false;
+function dispProjMenu() {
+  //create variable w to hold browser width.  Use client width for IE8 and earlier  
+  var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  //create variable x to get projects element
+  var x = document.getElementById("projects");
+  
+  //if client width less than or equal to 980 (media query break width to stack navigation links) run code for clickable menu.  If over 980 do not use clickable menu
+  if (w <= 980){  
+    //check state of clickable menu.  If false open menu on click, if true close menu on click
+    if (dispFlag == false){
+      x.style.display = "block";   
+      x.style.marginTop = 15 + "px";
+      x.style.maxWidth = 100 + "%";
+      dispFlag = true;
+    }else {
+      x.style.display = "none";
+      dispFlag = false;
+    }
+  }
+}
+
+//TBD menu, takes argument
+function dispTbdMenu(y) {
+  let tbd = y;
+  //create variable w to hold browser width.  Use client width for IE8 and earlier  
+  var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+ 
+  //create variable x to get projects element
+  var x = document.getElementById(tbd);
+  
+  //if client width less than or equal to 980 (media query break width to stack navigation links) run code for clickable menu.  If over 980 do not use clickable menu
+  if (w <= 980){  
+  
+    //check state of clickable menu.  If false open menu on click, if true close menu on click
+    if (dispFlag == false){
+      x.style.display = "block";   
+      x.style.marginTop = 15 + "px";
+      x.style.maxWidth = 100 + "%";
+      dispFlag = true;
+    }else {
+      x.style.display = "none";
+      dispFlag = false;
+    }
   }
 }
 
