f0135206fd3ba0381b6480db40dd623a1508b532
Cwell21 <cwe11@yahoo.com>
2018-06-10 08:31:36 -0600

    Modified the line chart to pull actual data from the web.  Function to resize chart based upon window resize not working.

--- js/script.js
+++ js/script.js
@@ -3,32 +3,32 @@
 //create variable to hold state of clickable menu
 var dispFlag = false;
 
-function dispUniv() {  
+function dispUniv() {
   let univ = ["uf", "fsu", "ucf", "usf", "fam", "fau", "fgc", "fiu", "ncf", "unf", "uwf", "fpu"];
   //get DIV element
   e = document.getElementById("partUniv");
-  var univIcon = "<li><img src=\"images\\univ\\" + univ[0] + ".jpg\" alt=\" " + univ[0] + "\"></li>" 
+  var univIcon = "<li><img src=\"images\\univ\\" + univ[0] + ".jpg\" alt=\" " + univ[0] + "\"></li>"
   for (i=1; i<univ.length; i++) {
-   univIcon = univIcon + "<li><img src=\"images\\univ\\" + univ[i] + ".jpg\" alt=\" " + univ[i] + "\"></li>" 
-  }    
+   univIcon = univIcon + "<li><img src=\"images\\univ\\" + univ[i] + ".jpg\" alt=\" " + univ[i] + "\"></li>"
+  }
   e.innerHTML = univIcon;
 }
 
 //Function for drop down menu, takes argument
 function dispMenu(y) {
   let dispMenu = y;
-  //create variable w to hold browser width.  Use client width for IE8 and earlier  
+  //create variable w to hold browser width.  Use client width for IE8 and earlier
   var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
- 
+
   //create variable x to get projects element
   var x = document.getElementById(dispMenu);
-  
+
   //if client width less than or equal to 980 (media query break width to stack navigation links) run code for clickable menu.  If over 980 do not use clickable menu
-  if (w <= 980){  
-  
+  if (w <= 980){
+
     //check state of clickable menu.  If false open menu on click, if true close menu on click
     if (dispFlag == false){
-      x.style.display = "block";   
+      x.style.display = "block";
       x.style.marginTop = 15 + "px";
       x.style.maxWidth = 100 + "%";
       dispFlag = true;
@@ -44,59 +44,77 @@ function dispMenu(y) {
 //GOOGLE CHARTS SCRIPT
 
 function dispLineChart(){
-  
-  var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-  var chartW = 800;
-  var chartH = 600;
-  
+
+  let w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  let chartW = 800;
+  let chartH = 600;
+
   if (w < 981){
     chartW = 225;
     chartH = 500;
   }
+  let chartSize = [chartW, chartH];
+  console.log("Chart Parameters : " + chartSize[0] + " x " + chartSize[1]);
+  return chartSize;
+}
+  google.charts.load('current', {'packages':['corechart']});
+  //Add new callback function
+  google.charts.setOnLoadCallback(getData);
 
-  
-  var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-  
-    google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
+function drawLineChart(newData) {
 
-    function drawChart() {
-      var data = google.visualization.arrayToDataTable([
-        ['Year', 'Energy Consumption', 'Renewable Energy Production'],
-        ['1960',  57344, 35680],
-        ['1970',  171346, 51035],
-        ['1980',  309694, 90049],
-        ['1990',  489741, 198986],
-        ['2000',  668216, 194952],
-        ['2010',  788887, 223518],
-        ['2015',  803865, 234192]
-      ]);
+    let displaySize = dispLineChart();
 
-      var options = {
-        title: 'Florida Electricity Consumption Vs Renewable Energy Production',
-        curveType: 'function',
-        legend: { position: 'bottom' },
-        backgroundColor: 'rgb(215,215,215)',
-        vAxis: {
-          title: 'Billion/BTU',
-        },
-        hAxis: {
-          title: 'Decade',
-        },
-      };
+    newData.unshift(["Year", "Energy Consumption"])
 
-      var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+    var data = google.visualization.arrayToDataTable(newData);
 
-      chart.draw(data, options);
-    }
+    var options = {
+      title: 'Florida Electricity Consumption',
+      width: displaySize[0],
+      height: displaySize[1],
+      curveType: 'function',
+      legend: { position: 'bottom' },
+      backgroundColor: 'rgb(215,215,215)',
+      vAxis: {
+        title: 'Billion/BTU',
+      },
+      hAxis: {
+        title: 'Year',
+      },
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+    chart.draw(data, options);
+  }
+
+function getData(){
+  //create new request object
+  let request = new XMLHttpRequest()
+
+  let requestUrl = "https://api.eia.gov/series/?api_key=07ee2112c06a1e33695c74d0f4d41f04&series_id=SEDS.TETCB.FL.A"
+
+  request.open('GET', requestUrl, true)
+
+  request.onload = function(){
+    let theActualData = JSON.parse(request.response).series[0].data
+    drawLineChart(theActualData)
+  }
+  //Callback for error handling
+  request.error = function(err){
+    console.log("error is: ", err)
+  }
+  //Send the request
+  request.send()
 }
 
 function dispBarChart() {
-  
+
   var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
   var chartW = chartW;
   var chartH = chartH;
-  
+
   if (w < 981){
     chartW = 265;
     chartH = 500;
@@ -146,7 +164,7 @@ function dispBarChart() {
           hAxis: {
             title: 'Billion/BTU',
           },
-          
+
         };
         var chart = new google.visualization.BarChart(document.getElementById("bar_chart"));
         chart.draw(view, options);
@@ -155,15 +173,13 @@ function dispBarChart() {
 
 
 function fescLoad(){
-    
-  dispUniv();   
-  dispLineChart();
+
+  dispUniv();
   dispBarChart();
+  drawLineChart();
+
 }
 
 window.addEventListener("resize", function() {
   fescLoad();
 });
-
-
-
